e537787e95cbcde7ec51bfe2e6750a7a06f11481
fioreale <alessio.fiorentino@hiop.io>
2024-09-29 09:45:53 +0200

    feat: fastapi does not expose static assets

--- src/myfitapp/main.py
+++ src/myfitapp/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-import os
 from myfitapp.api.endpoints import router
 
 app = FastAPI()
+
 app.include_router(router)
 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-public_dir = os.path.join(current_dir, "public")
-
-app.mount("/", StaticFiles(directory=public_dir, html=True))
+@app.get("/")
+async def root():
+    return {"message": "MyFitApp API is running"}
